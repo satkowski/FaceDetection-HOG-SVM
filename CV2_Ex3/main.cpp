31b25be9bccfaca12badd45185399abfbd695e69
Marc Satkowski <marc.satkowski@yahoo.de>
2015-07-08 17:16:43 +0200

    Kleinere Änderungen - Arrays zu vectoren umgewandelt - Hilfs Anweisung repariert

--- CV2_Ex3/main.cpp
+++ CV2_Ex3/main.cpp
@@ -7,12 +7,13 @@ using namespace cv;
 int main(int argc, const char** argv)
 {
 	// Creating a keymap for all the arguments that can passed to that programm
-	const String keyMap = "{help h ? usage  |   | show this message"
-						  "{pos p positive	| . | path for the positiv images}"
-						  "{neg n negative  | . | path for the negativ images}";
+	const String keyMap = "{help h usage ?  |   | show this message}"
+						  "{pos p positive  |   | path for the positiv images}"
+						  "{neg n negative  |   | path for the negativ images}";
 
 	// Reading the calling arguments
 	CommandLineParser parser(argc, argv, keyMap);
+	parser.about("FaceDetection-HOG-SVM");
 
 	if (parser.has("help"))
 	{
@@ -29,34 +30,33 @@ int main(int argc, const char** argv)
 	glob(negativePath, negativeFileNames);
 
 	// Testing if there are images in the pathes
-	if (positiveFileNames.size <= 0)
+	if (positiveFileNames.size() <= 0)
 	{
 		printf("There are no images in %s\n", positivePath);
 		return -1;
 	}
-	if (negativeFileNames.size <= 0)
+	if (negativeFileNames.size() <= 0)
 	{
 		printf("There are no images in %s\n", negativePath);
 		return -1;
 	}
 	srand(static_cast<unsigned>(time(0)));
 
-	Mat positiveImages[positiveFileNames.size], negativeImages[negativeFileNames.size];
-	HOGDescriptor positiveHOGs[positiveFileNames.size], negativeHOGs[negativeFileNames.size];
-	float positiveLabels[positiveFileNames.size] = 1.0, negativeLabels[negativeFileNames.size] = 1.0;
+	std::vector<Mat> positiveImages, negativeImages;
+	std::vector<HOGDescriptor> positiveHOGs, negativeHOGs;
 
 	// Converting the positve images and calculating the HOG
-	for (std::vector<String>::iterator fileName = positiveFileNames.begin(), int c = 0; fileName != positiveFileNames.end(); ++fileName, c++)
+	for (std::vector<String>::iterator fileName = positiveFileNames.begin(); fileName != positiveFileNames.end(); ++fileName)
 	{
 		Mat actualImage = imread(*fileName);
 
 		// Testing if the file is an image
-		if (actualImage.empty)
+		if (actualImage.empty())
 		{
 			printf("Couldn't read the image %s\n", *fileName);
 			return -1;
 		}
-		positiveImages[c] = actualImage;
+		positiveImages.push_back(actualImage);
 		cvtColor(actualImage, actualImage, CV_BGR2GRAY);
 
 
@@ -67,13 +67,12 @@ int main(int argc, const char** argv)
 		std::vector<Point> locations;
 		actualHogD.compute(actualImage, descriptorsValues, Size(0, 0), Size(0, 0), locations);
 
-		positiveHOGs[c] = actualHogD;
+		positiveHOGs.push_back(actualHogD);
 	}
 
 	// Calculating the HOG of the negativ images
-	for (std::vector<String>::iterator fileName = positiveFileNames.begin(), int c = 0; fileName != positiveFileNames.end(); ++fileName, c++)
+	for (std::vector<String>::iterator fileName = positiveFileNames.begin(); fileName != positiveFileNames.end(); ++fileName)
 	{
-
 	}
 	
 }
